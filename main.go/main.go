8bafe65a9534f446b75d1029cecb23a6072b5643
Doga <dogakaptanoglu@gmail.com>
2023-11-08 14:45:39 +0300

    added getAlbumByID function and error handling

--- main.go/main.go
+++ main.go/main.go
@@ -18,8 +18,12 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums) //Why do we pass the name of the function??
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
-	router.Run("localhost:8080")
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
 }
 
 // albums slice to seed record album data. (containing data you’ll use to start)
@@ -48,4 +52,20 @@ func postAlbums(c *gin.Context) {
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
